bfcd6a0a6a7fdeb239132b08f497b2eb0dc4e798
dheeraj2003 <97089373+Dheeraj10002000@users.noreply.github.com>
2022-06-03 03:51:57 +0000

    p6

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,14 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+fname=input("enter the file name")
+fhand = open('fname')
+count = 0
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[1])
+    count += 1
+print('There were %d lines in the file with From as the first word' % count)
+# mbox-short.txt 
